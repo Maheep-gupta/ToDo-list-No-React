91b672cad261b3a7d6d1a740b5e5c7f2dfe18f91
Maheep Gupta <91566211+CodingStuffsofMahee@users.noreply.github.com>
2023-04-11 02:21:00 +0530

    checkbox done

---
+++ public/index.js
@@ -0,0 +1,9 @@
+var checkbox=document.getElementById("flexCheckChecked").ariaChecked
+// checked elements Array
+var checked=[];
+
+if (checkbox) {
+    
+    checked.push
+    console.log("ji")
+}
--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const express = require('express')
 const ejs = require('ejs');
 const bodyParser = require('body-parser')
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'))
 
 
 app.get("/", function (req, res) {
     // res.send("HI")
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname + "/index.html");
+    console.log(req.body)
 }
 );
 
-// app.post("/", function (req, res) {
-    
-// }
-// );
+app.post("/", function (req, res) {
+    console.log(req.body.task)
+    res.send("recived")
+
+}
+);
 
 
 
